7893b5f73138c51709eb6016798b3c48c49628f8
Chris Zielinski <ckz8780@gmail.com>
2018-06-16 03:08:01 +0000

    Added app-level urls and includes

--- auth_with_django/urls.py
+++ auth_with_django/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from accounts.views import index, logout, login, registration, profile
+from django.urls import path, include
+from accounts.views import index
+from accounts import urls as accounts_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('accounts/logout/', logout, name='logout'),
-    path('accounts/login/', login, name='login'),
-    path('accounts/registration/', registration, name='registration'),
-    path('accounts/profile/', profile, name='profile'),
+    path('accounts/', include(accounts_urls)),
 ]
